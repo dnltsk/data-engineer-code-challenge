e2a0f3535d44f49355654f1b70e497de8ee6236c
dnltsk <daniel.teskle@gmail.com>
2018-11-21 23:06:19 +0100

    :sparkles: application startup fails if db initialization fails

--- src/main/java/org/dnltsk/d2d/challenge/Application.java
+++ src/main/java/org/dnltsk/d2d/challenge/Application.java
@@ -1,13 +1,16 @@
 package org.dnltsk.d2d.challenge;
 
+import lombok.extern.slf4j.Slf4j;
 import org.dnltsk.d2d.challenge.write.DbInitiator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
+import java.sql.SQLException;
 
 @SpringBootApplication
+@Slf4j
 public class Application {
 
 	@Autowired
@@ -15,7 +18,13 @@ public class Application {
 
 	@PostConstruct
 	public void initDatabase(){
-		dbInitiator.resetDb();
+		try {
+			dbInitiator.resetDb();
+		} catch (SQLException e) {
+			log.error("Cannot reset database -> Exiting Application!");
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 
 	public static void main(String[] args) {
